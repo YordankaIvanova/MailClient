733ce4966006b587c1e646b0b51ff0947cb731bb
Dani Ivanova <yordanka.ivanova77@gmail.com>
2017-05-22 21:17:44 +0300

    Added initial functionality for writing email

--- src/main/resources/public/functions.js
+++ src/main/resources/public/functions.js
@@ -69,3 +69,20 @@ function getParameterByName(name, url) {
     if (!results[2]) return null;
     return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
+
+function serializeObject(form) {
+	var o = {};
+	var a = form.serializeArray();
+	$.each(a, function() {
+		if (o[this.name]) {
+			if (!o[this.name].push) {
+				o[this.name] = [o[this.name]];
+			}
+			o[this.name].push(this.value || '');
+		} else {
+			o[this.name] = this.value || '';
+		}
+	});
+	return o;
+};
+
